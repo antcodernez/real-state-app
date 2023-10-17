178393c3c6dc1d397f38a756651e639ac0100470
Antonio Jiménez <marshday5@gmail.com>
2023-10-16 22:45:33 -0600

    Configuring the file .env

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 import express  from "express"; //ES6+ 
 import usuarioRouter from "./routing/usuarioRoutes.js";
 import db from "./config/bd.js";
-
+import config from "./config/config.cjs";
 //Crear la app
 const app = express();  
 const port = 4000;  
-
-//coneccion a la db
-try 
-    {
-        await db.authenticate();
-        console.log("Conexión correcta a la bd");
-    } 
-catch (error) 
-    {
-        console.error(error);
-    }
 
 //Habilitando pug
 // .set() es para agregar configuraciones al proyecto
